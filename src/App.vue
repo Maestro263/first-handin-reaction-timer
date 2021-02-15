e2d2c0223ba150af559501a7536ae1b600e316aa
mo263 <mohammedhassani1995@gmail.com>
2021-02-15 02:10:30 +0100

    initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,37 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+<h1>My Reaction Timer</h1>
+<button @click="begin" :disabled="isPlaying">Begin the Game</button>
+<block v-if="isPlaying" :delay="delay" @end="gameOver"/>
+<results v-if="showResults" :score="score" />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import block from './components/block.vue'
+import results from './components/results.vue'
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  components: { block, results },
+  data(){
+    return{
+      isPlaying: false,
+      delay: null,
+      score: null,
+      showResults: false
+    }
+  },
+  methods: {
+    begin(){
+      //Between 2-7 seconds
+      this.delay = 2000 + Math.random() * 5000,
+      this.isPlaying = true
+      this.showResults = false
+    },
+    gameOver(reactionTime){
+      this.score = reactionTime
+      this.isPlaying = false
+      this.showResults = true
+    }
   }
 }
 </script>
@@ -20,7 +42,25 @@ export default {
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
+  color: teal;
   margin-top: 60px;
+}
+*{
+  background: yellow;
+}
+button{
+  background: blue;
+  color: white;
+  border: none;
+  padding: 8px 16px;
+  border-radius: 4px;
+  font-size: 16px;
+  letter-spacing: 1px;
+  cursor: pointer;
+  margin: 10px;
+}
+button[disabled]{
+  opacity: 0.2;
+  cursor: not-allowed;
 }
 </style>
---
+++ src/components/results.vue
@@ -0,0 +1,33 @@
+<template>
+  <p>Reaction Time - {{ score }} ms</p>
+  <p class="rank">{{ rank }}</p>
+</template>
+
+<script>
+export default {
+    props: ['score'],
+    data(){
+        return{
+            rank: null
+        }
+    },
+    mounted(){
+        if(this.score < 250){
+            this.rank = 'Usain Bolt Speed!'
+        } else if(this.score < 400){
+            this.rank = 'Fast Reaction'
+        } else{
+            this.rank = 'Too slow....'
+        }
+    }
+}
+</script>
+
+<style>
+    .rank{
+        font-size: 1.4em;
+        color: teal;
+        font-weight: bold;
+    }
+
+</style>
